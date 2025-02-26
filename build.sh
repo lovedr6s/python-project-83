2f2692935411d6abe7ce677af04a7e875a9ec8e3
lovedr6s <praense@icloud.com>
2025-02-26 16:06:48 +0300

    changed build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-. $HOME/.local/bin/env
+source $HOME/.local/bin/env
 
-source
-
-make install && psql -a -d $DATABASE_URL -f database.sql
+make install
