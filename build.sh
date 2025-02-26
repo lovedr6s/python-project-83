5defe15b2c9312919ce867964f34d6de970c51ea
lovedr6s <praense@icloud.com>
2025-02-26 17:13:57 +0300

    fmoreifgnviermg

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+# Устанавливаем uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
+# Добавляем uv в PATH
 source $HOME/.local/bin/env
 
+# Создаем виртуальное окружение
 uv venv .venv
+
+# Активируем виртуальное окружение
 source .venv/bin/activate
-uv pip install .
+
+# Устанавливаем зависимости из pyproject.toml
+uv sync
