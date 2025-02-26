0d8eaa5b40fb854e52b40bcaf938c007cde01a99
lovedr6s <praense@icloud.com>
2025-02-26 17:17:45 +0300

    fmoreifgnviermg

--- Makefile
+++ Makefile
@@ -1,15 +1,20 @@
+install:
+	uv sync
 dev:
 	uv run flask --debug --app page_analyzer:app run
+
+lint:
+	uv run flake8 page_analyzer
+
+format:
+	uv run black page_analyzer
 
 PORT ?= 8000
 start:
 	uv run gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
 
-build:
-	./build.sh
-
 render-start:
 	gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
 
-install:
-	uv sync
+build:
+	./build.sh
--- build.sh
+++ build.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
-
-# Устанавливаем uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
-
-# Добавляем uv в PATH
-export PATH="$HOME/.local/bin:$PATH"
-
-# Создаем виртуальное окружение
-uv venv .venv
-
-# Активируем виртуальное окружение
-source .venv/bin/activate
-
-# Устанавливаем зависимости из pyproject.toml
-uv sync
+source $HOME/.local/bin/env
+make install && psql -a -d $DATABASE_URL -f database.sql
