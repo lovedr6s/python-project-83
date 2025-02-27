0b603c99697fdc8b8775141b4cc6147b60a4182d
lovedr6s <praense@icloud.com>
2025-02-27 16:07:20 +0300

    some new stuff heheheeh

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,14 +1,12 @@
-from dotenv import load_dotenv
 from flask import Flask, render_template, redirect, url_for, request, flash
-from database import *
+from page_analyzer.database import *
+from page_analyzer.utils import SECRET_KEY
 from urllib.parse import urlparse
 import validators
-import os
 
-load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.get('/')
 def index():
@@ -24,8 +22,8 @@ def add_url():
     
     parsed_url = urlparse(url)
     normalized_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
-
     existing_url = get_id_by_name(normalized_url)
+
     if existing_url:
         flash("Такой url уже существует")
         return redirect(url_for('show_url', id=existing_url[0]))
@@ -37,20 +35,24 @@ def add_url():
 
 @app.route("/urls")
 def show_urls():
-    urls = get_all_urls()
-    return render_template('urls.html', urls=urls)
+    return render_template('urls.html', urls=get_all_urls())
 
 @app.get('/urls/<int:id>')
-def show_url(id):
+def url_detail(id):
     url_data = get_url_by_id(id)
-    return render_template('url_detaly.html', url=url_data[1])
+    if url_data:
+        url_check = get_data_from_url_checks_by_id(url_data[0])
+    return render_template('url_detaly.html', url_check_data=url_check, url=url_data[1], id=url_data[0])
+
+@app.get('/urls/<int:id>/details')
+def show_url(id):
+    return redirect(url_for('url_detail', id=id))
 
 
 @app.post('/urls/<int:id>/checks')
 def check_url(id):
-    with conn.cursor() as curs:
-        curs.execute("INSERT INTO url_checks (created_at) VALUES (%s)", (datetime.datetime.now()))
-
+    insert_data_into_url_checks(id)
+    flash('Страница успешно проверенна')
     return redirect(url_for('show_url', id=id))
     
    
--- page_analyzer/database.py
+++ page_analyzer/database.py
@@ -1,5 +1,5 @@
 import psycopg2
-from utils import DATABASE_URL
+from page_analyzer.utils import DATABASE_URL
 import datetime
 
 conn = psycopg2.connect(DATABASE_URL)
@@ -8,7 +8,7 @@ def get_url_by_id(id):
     with conn.cursor() as curs:
         curs.execute("SELECT * FROM urls WHERE id = %s", (id, ))
         data = curs.fetchone()
-        return data[1]
+        return data
 
 def get_id_by_name(name):
     with conn.cursor() as curs:
@@ -17,12 +17,24 @@ def get_id_by_name(name):
 
 def get_all_urls():
     with conn.cursor() as curs:
-        curs.execute("SELECT * FROM urls ORDER BY id")
+        curs.execute("SELECT DISTINCT ON (urls.id) urls.id, urls.name, url_checks.created_at FROM urls INNER JOIN url_checks ON url_checks.url_id = urls.id WHERE url_checks.created_at = ( SELECT created_at FROM url_checks ORDER BY created_at DESC LIMIT 1 ) ORDER BY id")
         return curs.fetchall()
     
 def insert_data_into_urls(data):
     with conn.cursor() as curs:
-        curs.execute('INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id', (data, datetime.datetime.now()))
+        curs.execute('INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id', (data, datetime.date.today()))
         id = curs.fetchone()[0]
         conn.commit()
         return id
+
+
+def insert_data_into_url_checks(data):
+    with conn.cursor() as curs:
+        curs.execute('INSERT INTO url_checks (url_id, created_at) VALUES (%s, %s)', (data, datetime.date.today()))
+        conn.commit()
+
+
+def get_data_from_url_checks_by_id(data):
+    with conn.cursor() as curs:
+        curs.execute('SELECT * FROM url_checks WHERE url_id = %s', (data, ))
+        return curs.fetchall()
