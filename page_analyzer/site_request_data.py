84be8aa6189800875471e4f0c296a2a05ee63eec
lovedr6s <praense@icloud.com>
2025-03-01 11:53:26 +0300

    yay

--- page_analyzer/site_request_data.py
+++ page_analyzer/site_request_data.py
@@ -15,14 +15,14 @@ def get_page_data(site):
     try:
         response = requests.get(site)
         response.raise_for_status()
-        soup = BeautifulSoup(response.text, features="html.parser")
-        title = soup.title.text.strip() if soup.title else ''
-
-        meta_description = soup.find('meta', attrs={'name': 'description'})
-        description = meta_description['content'] if meta_description else ''
-        h1 = soup.find('h1').text.strip() if soup.find('h1') else ''
-        return {'title': title,
-                'h1': h1,
-                'description': description}
     except Exception:
         return 'error'
+    soup = BeautifulSoup(response.text, features="html.parser")
+    title = soup.title.text.strip() if soup.title else ''
+
+    meta_description = soup.find('meta', attrs={'name': 'description'})
+    description = meta_description['content'] if meta_description else ''
+    h1 = soup.find('h1').text.strip() if soup.find('h1') else ''
+    return {'title': title,
+            'h1': h1,
+            'description': description}
