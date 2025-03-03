39ac544f8268f891c0a884dc31827bcce3b7bbe4
lovedr6s <praense@icloud.com>
2025-03-03 16:29:04 +0300

    added source activation

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
-
+which uv
 source $HOME/.local/bin/env
 export PATH="/opt/render/.local/bin:$PATH"
 source .venv/bin/activate
-
+which uv
 make install  
 
 psql -a -d "$DATABASE_URL" -f database.sql
