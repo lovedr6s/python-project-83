31267b2f835f0b43846d6a364454ba882ddcb28c
lovedr6s <praense@icloud.com>
2025-03-04 18:27:42 +0300

    lol

--- database.sql
+++ database.sql
@@ -1,10 +1,10 @@
-CREATE TABLE urls (
+CREATE TABLE IF NOT EXISTS urls (
     id SERIAL PRIMARY KEY,
     name TEXT UNIQUE NOT NULL,
     created_at DATE,
     status_code INTEGER);
 
-CREATE TABLE url_checks (
+CREATE TABLE IF NOT EXISTS url_checks (
     id SERIAL PRIMARY KEY,
     url_id INTEGER,
     status_code INTEGER,
